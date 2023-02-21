82c87ff2dfc4655e024378477b325699471f287a
John Lannon <jlannon@gmail.com>
2023-02-21 12:47:01 -0600

    Footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        <a target="_blank" href="https://www.netlify.com/"> netlify</a>
       </footer>
     </>
   )
